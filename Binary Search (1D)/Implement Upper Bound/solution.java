069b150e8496085fdfc31f2c66fd01e1f3b641e3
theadityaroy <adityaroy2308@hotmail.com>
2023-10-06 09:54:06 +0530

    Initial commit

--- Binary Search (1D)/Implement Upper Bound/solution.java
+++ Binary Search (1D)/Implement Upper Bound/solution.java
@@ -1,17 +1,17 @@
 // Problem Statement:
 
-// Given a sorted array of N integers and an integer x, write a program to find the lower bound of x.
+// Given a sorted array of N integers and an integer x, write a program to find the upper bound of x.
 
 public class solution {
-    public static int lowerBound(int []arr, int n, int x) {
+    public static int upperBound(int []arr, int x, int n){
         int low = 0;
         int high = n - 1;
         int res = n;
 
-        while (low <= high) {
+        while (low <= high){
             int mid = (low + high) / 2;
 
-            if (arr[mid] >= x) {
+            if (arr[mid] > x) {
                 res = mid;
                 high = mid - 1;
             }

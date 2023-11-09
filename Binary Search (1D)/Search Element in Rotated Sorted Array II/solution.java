d1fe042f2303ba25a9211e870058deb05db61292
theadityaroy <adityaroy2308@hotmail.com>
2023-11-09 13:13:05 +0530

    Initial commit

---
+++ Binary Search (1D)/Search Element in Rotated Sorted Array II/solution.java
@@ -0,0 +1,43 @@
+// Problem Statement:
+
+// Given an integer array arr of size N, sorted in ascending order (may contain duplicate values)
+// and a target value k. Now the array is rotated at some pivot point unknown to you.
+// Return True if k is present and otherwise, return False. 
+
+public class solution {
+    public static boolean searchInARotatedSortedArrayII(int []A, int key) {
+        int n = A.length;
+        int low = 0;
+        int high = n - 1;
+
+        while(low <= high) {
+            int mid = (low + high)/ 2;
+
+            if(A[mid] == key) {
+                return true;
+            }
+
+            // Checking Left Half
+            if(A[low] <= A[mid]) {
+                if(A[low] <= key && key <= A[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+
+            // Checking Right Half
+            else {
+                if(A[mid] <= key && key <= A[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+        
+        return false;
+    }
+}

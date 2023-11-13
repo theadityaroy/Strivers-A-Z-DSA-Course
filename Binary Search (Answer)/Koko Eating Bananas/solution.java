c2634d34a049610f7dca94c57e12f3d00ae51e4e
theadityaroy <adityaroy2308@hotmail.com>
2023-11-13 21:42:51 +0530

    Initial commit

--- Binary Search (Answer)/Koko Eating Bananas/solution.java
+++ Binary Search (Answer)/Koko Eating Bananas/solution.java
@@ -2,6 +2,11 @@
 
 // A monkey is given ‘n’ piles of bananas, whereas the ‘ith’ pile has ‘a[i]’ bananas.
 // An integer ‘h’ is also given, which denotes the time (in hours) for all the bananas to be eaten.
+// Each hour, the monkey chooses a non-empty pile of bananas and eats ‘k’ bananas.
+// If the pile contains less than ‘k’ bananas, then the monkey consumes all the bananas and
+// won’t eat any more bananas in that hour.
+
+// Find the minimum number of bananas ‘k’ to eat per hour so that the monkey can eat all the bananas within ‘h’ hours.
 
 public class solution {
     public static int minimumRateToEatBananas(int[] v, int h) {
